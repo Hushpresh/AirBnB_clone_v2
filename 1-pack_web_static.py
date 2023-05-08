c541e0c467cab22e0efc11385f7f0f9686557e89
Hush <preciouschinazaekpere6@gmail.com>
2023-05-08 06:18:25 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/usr/bin/python3
-"""Compress web static package
-"""
-from fabric.api import local
+
+""" This module contains the function do_pack that generates a .tgz archive
+
+  from the contents of the web_static folder (fabric script) """
+
+from fabric.api import *
+
 from datetime import datetime
 
-
 def do_pack():
-    """Function to compress directory
 
-    Return: path to archive on success; None on fail
-    """
-    # Get current time
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
+    """ Fabric script that generates a .tgz archive from the contents of the...
 
-    # Create archive
-    local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
+    ...web_static folder """
 
-    # Check if archiving was successful
+    local("sudo mkdir -p versions")
+
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    filename = "versions/web_static_{}.tgz".format(date)
+
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+
     if result.succeeded:
-        return archive_path
-    return None
+
+        return filename
+
+    else:
+
+        return None
+
+
+
+
+
+
+
+
+
